c8d888d8b276535a315395381e160ed1bdf5f89b
kuromaaaaa <kuromaxa@gmail.com>
2023-08-30 05:38:28 +0900

    ラムダ式、linqを使ってオートエイムを追加

--- Assets/script/EnemySuperClass.cs
+++ Assets/script/EnemySuperClass.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public abstract class EnemySuperClass : MonoBehaviour
 {
     public abstract void Damage();
+    cross _cross;
+    void OnEnable()
+    {
+        _cross = GameObject.FindObjectOfType<cross>();
+        _cross.Enemy.Add(this.gameObject);
+    }
+
+    private void OnDisable()
+    {
+        _cross.Enemy.Remove(this.gameObject);
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
--- Assets/script/cross.cs
+++ Assets/script/cross.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class cross : MonoBehaviour
@@ -7,17 +8,36 @@ public class cross : MonoBehaviour
     [SerializeField] bool mouseCursor = true;
     bool controller = true;
 
-    GameObject player;
+    [SerializeField] bool _godMode;
+    GameObject _auto = default;
+
+    GameObject _player;
+    List<GameObject> _enemys = new List<GameObject>();
+    public List<GameObject> Enemy
+    {
+        get { return _enemys; }
+        set { _enemys = value; }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = mouseCursor;
-        player = GameObject.Find("プレイヤー");
+        _player = GameObject.Find("プレイヤー");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(_enemys.Count);
+        _auto = _enemys.OrderBy(x => Vector2.Distance(_player.transform.position, x.transform.position)).FirstOrDefault();
+        if (_godMode)
+        {
+            this.transform.position = _auto.transform.position;
+            this.transform.position = new Vector3(transform.position.x,transform.position.y,0);
+            return;
+        }
         if(Input.GetButtonDown("start"))
         {
             controller = true;
@@ -34,7 +54,7 @@ public class cross : MonoBehaviour
         }
         if(controller == true)
         {
-            this.transform.position = new Vector3(Input.GetAxisRaw("RHorizontal") *3 +player.transform.position.x , -Input.GetAxisRaw("RVertical") *3 +player.transform.position.y ,0);
+            this.transform.position = new Vector3(Input.GetAxisRaw("RHorizontal") *3 +_player.transform.position.x , -Input.GetAxisRaw("RVertical") *3 +_player.transform.position.y ,0);
         }
     }
 }
